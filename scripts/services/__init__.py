93b5edb68661a477ebd86a1980e28989c2e23e42
dummy-bishwadeep <146086665+dummy-bishwadeep@users.noreply.github.com>
2024-07-28 02:08:21 +0530

    Delete scripts directory

--- scripts/services/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from scripts.services import logbooks, table, new_datagrid
-
-router = APIRouter()
-router.include_router(logbooks.router)
-router.include_router(table.table)
-router.include_router(new_datagrid.datagrid)
